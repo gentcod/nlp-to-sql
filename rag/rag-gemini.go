9627f036fc29040f9ba59923a5a5f7fbe268cb29
Oyefule <drelanorgent@gmail.com>
2025-05-29 00:48:09 +0100

    [chore]: Modified LLM RAG context logic

--- rag/rag-gemini.go
+++ rag/rag-gemini.go
@@ -25,71 +25,32 @@ func (llm *GeminiLLM) GenerateQuery(que string) (string, error) {
 	ctx := context.Background()
 	client, err := genai.NewClient(ctx, option.WithAPIKey(llm.Opts.ApiKey))
 	if err != nil {
-		return llm.Query, nil
+		return llm.Query, err
 	}
 	defer client.Close()
 
 	model := client.GenerativeModel(llm.Opts.Model)
-	cs := model.StartChat()
-
-	cs.History = []*genai.Content{
-		{
-			Parts: []genai.Part{
-				genai.Text("Generate a sql query from the next stream of input or text"),
-			},
-			Role: "user",
-		},
-		{
-			Parts: []genai.Part{
-				genai.Text("Only SELECT queries or queries to read data should be generated"),
-			},
-			Role: "user",
-		},
-		{
-			Parts: []genai.Part{
-				genai.Text(fmt.Sprintf("The Schema for the database is in the form: %v", llm.Opts.Context)),
-			},
-			Role: "user",
-		},
-		{
-			Parts: []genai.Part{
-				genai.Text("Only queries based on the database schema should be generated"),
-			},
-			Role: "user",
-		},
-		{
-			Parts: []genai.Part{
-				genai.Text("Omit fields or columns with sensitive data such as password, hashed_password or similar fields no matter the condtions stated in corresponding statements."),
-			},
-			Role: "user",
-		},
-		{
-			Parts: []genai.Part{
-				genai.Text("If none of the conditions are satisfied, return a custom error response"),
-			},
-			Role: "user",
-		},
-		{
-			Parts: []genai.Part{
-				genai.Text("If all the conditions are satisfied, return only the SQL query as a response"),
-			},
-			Role: "user",
-		},
-		{
-			Parts: []genai.Part{
-				genai.Text("Return the respone as a plain text rather than a block of code and remove the indentations~"),
-			},
-			Role: "user",
-		},
+	parts := []genai.Part{
+		genai.Text("Generate a sql query from the next stream of input or text"),
+		genai.Text("Only SELECT queries or queries to read data should be generated"),
+		genai.Text(fmt.Sprintf("The Schema for the database is in the form: %v", llm.Opts.Context)),
+		genai.Text("Only queries based on the database schema should be generated"),
+		genai.Text("Omit fields or columns with sensitive data such as password, hashed_password or similar fields no matter the condtions stated in corresponding statements."),
+		genai.Text("If none of the conditions are satisfied, return a custom error response"),
+		genai.Text("If all the conditions are satisfied, return only the SQL query as a response"),
+		genai.Text("Return the respone as a plain text rather than a block of code and remove the indentations~"),
 	}
+
+	model.SystemInstruction = genai.NewUserContent(parts...)
+	cs := model.StartChat()
 
 	res, err := cs.SendMessage(ctx, genai.Text(que))
 	if err != nil {
-		return llm.Query, nil
+		return llm.Query, err
 	}
 	llm.Query, err = getGeminiResponse(res)
 	if err != nil {
-		return llm.Query, nil
+		return llm.Query, err
 	}
 
 	return llm.Query, nil
@@ -99,41 +60,27 @@ func (llm *GeminiLLM) GenerateResponse(data interface{}, que string) (string, er
 	ctx := context.Background()
 	client, err := genai.NewClient(ctx, option.WithAPIKey(llm.Opts.ApiKey))
 	if err != nil {
-		return llm.Response, nil
+		return llm.Response, err
 	}
 	defer client.Close()
 
 	model := client.GenerativeModel(llm.Opts.Model)
-	cs := model.StartChat()
-
-	cs.History = []*genai.Content{
-		{
-			Parts: []genai.Part{
-				genai.Text("Generate a summary from the next stream of input or text"),
-			},
-			Role: "user",
-		},
-		{
-			Parts: []genai.Part{
-				genai.Text(fmt.Sprintf("Use these data: %v retrieved from the database in a conversational manner", data)),
-			},
-			Role: "user",
-		},
-		{
-			Parts: []genai.Part{
-				genai.Text(fmt.Sprintf("Use this as context for the data returned: %v", que)),
-			},
-			Role: "user",
-		},
+	parts := []genai.Part{
+		genai.Text("Generate a summary from the next stream of input or text"),
+		genai.Text(fmt.Sprintf("Use these data: %v retrieved from the database in a conversational manner", data)),
+		genai.Text(fmt.Sprintf("Use this as context for the data returned: %v", que)),
 	}
+
+	model.SystemInstruction = genai.NewUserContent(parts...)
+	cs := model.StartChat()
 
 	res, err := cs.SendMessage(ctx, genai.Text("What is the summary of the data?"))
 	if err != nil {
-		return llm.Response, nil
+		return llm.Response, err
 	}
 	llm.Response, err = getGeminiResponse(res)
 	if err != nil {
-		return llm.Response, nil
+		return llm.Response, err
 	}
 
 	return llm.Response, nil
